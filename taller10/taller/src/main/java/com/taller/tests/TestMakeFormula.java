4138230fb649b86737552f8bfb1b155403a25da0
Juan Miñaca <jminaca@espol.edu.ec>
2025-07-30 16:26:41 -0500

    Tallerproyecto10

---
+++ taller10/taller/src/main/java/com/taller/tests/TestMakeFormula.java
@@ -0,0 +1,32 @@
+package com.taller.tests;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+
+public class TestMakeFormula {
+    
+   @Test
+   @DisplayName("Haciendo pruebas de MakeFormula")
+
+   
+    public void testMakeFormula() {
+        String formula = Operations.MakeFormula();
+        
+        @Test 
+        @DisplayName("Verificar que en la formula no este vacia o sea nula")
+        assertNotNull(formula);
+        assertFalse(formula.isEmpty());
+        
+        @Test
+        @DisplayName("Se verifica que se esten usando los operadores permitidos")
+        assertTrue(formula.matches(".*[+\\-*/].*"), "La fórmula debe contener al menos un signo de operador");
+        
+        @Test 
+        @DisplayName("Se verifica que los caracteres ingresados sean validos")
+        assertTrue(formula.matches("[0-9+\\-*/]+"), "La fórmula contiene caracteres invalidos");
+        
+        System.out.println("Fórmula generada: " + formula);
+    }
+
+    
+}
