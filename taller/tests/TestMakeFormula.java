026a44a844d3fa303e46debe5a3a2f2597c1ccf0
Abel Carrera <117789929+theabelchannel@users.noreply.github.com>
2025-07-30 16:22:08 -0500

    Update TestMakeFormula.java

--- taller/tests/TestMakeFormula.java
+++ taller/tests/TestMakeFormula.java
@@ -1,5 +1,5 @@
 package taller.tests;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.tests;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 
