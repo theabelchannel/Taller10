cc90e5fff75dff253490af3ed3b37978fd14c255
Valery1302 <142460056+Valery1302@users.noreply.github.com>
2025-07-30 16:17:15 -0500

    Update TestSolve.java

--- taller/tests/TestSolve.java
+++ taller/tests/TestSolve.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package taller;
+package taller.tests;
 import org.junit.jupiter.api.tests;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
