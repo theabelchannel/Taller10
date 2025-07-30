702ce851e684839243346f0c3432629aadae333c
Abel Carrera <117789929+theabelchannel@users.noreply.github.com>
2025-07-30 16:18:39 -0500

    Update TestSolve.java

--- taller/tests/TestSolve.java
+++ taller/tests/TestSolve.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package taller.tests;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
